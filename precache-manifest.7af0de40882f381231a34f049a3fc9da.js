c9468c71149abd902c889f14d97e3334f5e2c524
lvladv <mr.lvladv@yandex.ru>
2020-02-02 14:19:39 +0300

    build

---
+++ precache-manifest.7af0de40882f381231a34f049a3fc9da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a594fb950e0574bab5e15d9ad3fb0d5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "948ac72ef70365637773",
+    "url": "./static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8e0c1850a1de2a8749fe",
+    "url": "./static/css/main.2012105b.chunk.css"
+  },
+  {
+    "revision": "948ac72ef70365637773",
+    "url": "./static/js/2.ef4db564.chunk.js"
+  },
+  {
+    "revision": "6aeb469c0c20fd3334c86de961c4692c",
+    "url": "./static/js/2.ef4db564.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8e0c1850a1de2a8749fe",
+    "url": "./static/js/main.9f4ca5f1.chunk.js"
+  },
+  {
+    "revision": "16c53c032ee348996a01",
+    "url": "./static/js/runtime-main.e0a84022.js"
+  }
+]);
